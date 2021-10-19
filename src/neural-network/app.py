9d1a7f3367fcef2e35daff0c956bb7c69648f4a2
HolzJa180417 <40146720180417@litec.ac.at>
2021-10-19 11:24:21 +0200

    loaded data and defined model

---
+++ src/neural-network/app.py
@@ -0,0 +1,48 @@
+import keras.optimizers
+import numpy as np
+from keras.datasets import mnist
+from matplotlib import pyplot
+from keras.models import Sequential
+from keras.layers import Dense
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Dense
+from keras.layers import Flatten
+from tensorflow.keras.optimizers import SGD
+
+def load_dataset():
+    # load dataset
+    (trainX, trainY), (testX, testY) = mnist.load_data()
+    # reshape dataset to have a single channel
+    trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
+    testX = testX.reshape((testX.shape[0], 28, 28, 1))
+    # one hot encode target values
+    trainY = tf.to_categorical(trainY)
+    testY = tf.to_categorical(testY)
+    return trainX, trainY, testX, testY
+
+# scale pixels
+def prep_pixels(train, test):
+    # convert from integers to floats
+    train_norm = train.astype('float32')
+    test_norm = test.astype('float32')
+    # normalize to range 0-1
+    train_norm = train_norm / 255.0
+    test_norm = test_norm / 255.0
+    # return normalized images
+    return train_norm, test_norm
+
+# define cnn model
+def define_model():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dense(10, activation='softmax'))
+    # compile model
+    opt = SGD(lr=0.01, momentum=0.9)
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
